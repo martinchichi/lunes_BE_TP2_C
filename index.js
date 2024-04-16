ad8ff49a111eb16f8f5f7858fa1b3498f1add969
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-04-15 22:46:53 -0300

    clase5 modulo fs

--- index.js
+++ index.js
@@ -1,59 +1,28 @@
-function uno(param) {
-  return param;
-}
+// const fs=require("node:fs")
+// console.log(`🚀 ~ fs:`, fs.readFileSync)
+const { readFileSync, writeFileSync } = require("node:fs");
+// console.log(`🚀 ~ readFileSync:`, readFileSync)
 
-setTimeout(() => {
-  console.log("set time out 1");
-}, 3000);
-
-setTimeout(() => {
-  console.log("set time out 2");
-}, 2000);
-
-function dos(param) {
-  return new Promise((resolve, reject) => {
-    if (param === 2) {
-      resolve("promesa ok es dos");
-    } else {
-      reject("Pomesa rechazada");
-    }
-  });
-}
-function tres() {
-  return fetch("http://swapi.dev/api/planets/1/");
-}
-async function cuatro() {
+function read(params) {
   try {
-    const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    if (data.status === 404) throw "Error 404";
-    const dataJson = await data.json();
-    // console.log(`🚀 ~ cuatro ~ data:`, dataJson);
-    return dataJson;
+    const data = readFileSync(`./${params}`, "utf-8");
+    const dataParse = JSON.parse(data);
+    dataParse.name = "viernes";
+    console.log(`🚀 ~ read ~ dataParse:`, dataParse);
   } catch (error) {
-    console.log(`🚀 ~ cuatro ~ error:`, error);
-  } finally {
-    console.log("final del try catch");
+    console.log(error);
   }
 }
-function cinco(param) {
-  return param;
+
+function write(params) {
+  try {
+    const data=writeFileSync("./data.txt", params)
+    console.log(`🚀 ~ write ~ data:`, data)
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-console.log(`🚀 ~ uno:`, uno("uno"));
-// console.log(`🚀 ~ dos:`, dos(2));
-
-dos(2)
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((e) => console.log(e))
-  .finally(() => console.log(`final de la promesa`));
-
-// console.log(`🚀 ~ tres:`, tres("tres"));
-tres()
-  // .then((data) => data.json())
-  // .then((res) => console.log(tres, res))
-  // .catch((e) => console.log(e));
-// console.log(`🚀 ~ cuatro:`, cuatro("cuatro"));
-cuatro().then(data=>console.log(data))
-console.log(`🚀 ~ cinco:`, cinco("cinco"));
+// read("package.json");
+write("[1,2,3]")
+write("Hola")
