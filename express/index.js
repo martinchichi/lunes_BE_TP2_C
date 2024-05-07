1444699c357bf5b9f192a7c22acaa9fa4c132592
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-05-06 23:04:32 -0300

    clase6 express

--- express/index.js
+++ express/index.js
@@ -1,20 +1,33 @@
-import express from "express"
-const app=express()
+import express from "express";
+const app = express();
 
-app.get("/",(req, res) => {
-  // console.log(`🚀 ~ app.get ~ req:`, req)
-   res.send("hola get desde express")
+// app.get("/", (req, res) => {
+//   // console.log(`🚀 ~ app.get ~ req:`, req)
+//   res.send("hola get desde express");
+// });
+app.post("/", (req, res) => {
+  res.send("hola post desde express");
+});
+app.delete("/delete", (req, res) => {
+  res.send("hola delete desde express");
+});
 
-})
-app.post("/",(req, res)=>{
-  res.send("hola post desde express")
-})
-app.delete("/delete",(req, res)=>{
-  res.send("hola delete desde express")
-})
+// ---------------------------
+let count = 0;
+app.get("/fecha", (req, res) => {
+  const fecha = Date();
+  res.send(fecha);
+});
+app.get("/count", (req, res) => {
+  count++;
+  res.send({ count });
+});
+app.use(express.static('files'));
+// app.get("/", (req, res) => {
+//   res.send("hola get desde express");
+// });
+// ---------------------------
 
-
-
-app.listen(8080,()=>{
-  console.log("express ok")
-})
+app.listen(8080, () => {
+  console.log("express ok");
+});
