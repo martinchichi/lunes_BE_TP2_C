48350b0052d85d1e14055686817d8a61089c3afb
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-05-13 20:06:49 -0300

    clase6

--- express/index.js
+++ express/index.js
@@ -1,5 +1,17 @@
 import express from "express";
 const app = express();
+import url from "url";
+import path from "path";
+
+// console.log(`🚀 ~ path:`, path)
+// console.log(`🚀 ~ url:`, url)
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(`🚀 ~ meta.url:`, import.meta.url);
+console.log(`🚀 ~ __filename:`, __filename);
+console.log(`🚀 ~ __dirname:`, __dirname);
 
 // app.get("/", (req, res) => {
 //   // console.log(`🚀 ~ app.get ~ req:`, req)
@@ -22,10 +34,14 @@ app.get("/count", (req, res) => {
   count++;
   res.send({ count });
 });
-app.use(express.static('files'));
-// app.get("/", (req, res) => {
-//   res.send("hola get desde express");
-// });
+// app.use("/html",express.static('files'));
+app.get("/:dir/:file", (req, res) => {
+  // console.log(`🚀 ~ app.get ~ req:`, req.params)
+  // const dir= req.params.dir
+  // const file= req.params.file
+  const { dir, file } = req.params;
+  res.sendFile(path.join(__dirname, dir, `${file}.html`));
+});
 // ---------------------------
 
 app.listen(8080, () => {
